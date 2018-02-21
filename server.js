01538c3af7cdab993f963cb975daf8db9da1a3a5
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-22 00:52:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,36 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article-1 | Rakesh',
+    heading: 'Article One',
+    content: `
+    <p>This a line about article one.</p>
+    <p>This a line about article one.</p>
+    `
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+    <head>
+    <title>${title}</title>
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+    <h1>${heading}</h1>
+    ${content}
+    <a href="/">Home</a>
+    </body>
+    </html>
+    
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +48,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
