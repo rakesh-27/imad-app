66b5ded8f71737fce6623c9cadfb1e4991ff676f
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-24 20:23:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var mar = 0;
+function moveRight(){
+    mar = mar+1;
+    img.style.marginLeft = mar + 'px';
+}
+var img = getElementById('madi');
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+}
