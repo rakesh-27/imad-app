bcddbc6ef3005b6bdb2d0e9f9422f9b61715cad0
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-24 22:03:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/counter', function(req, res){
 var names = [];
 app.get('/submit-name', function(req, res){
     var name = req.query.name;
-    names.push[name];
+    names.push(name);
     res.send(JSON.stringify(names));
 });
 
