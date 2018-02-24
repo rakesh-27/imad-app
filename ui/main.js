81f366e25eac94c7c125a9c89103b4d7520b1882
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-24 21:11:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,4 +7,19 @@ function moveRight(){
 var img = document.getElementById('madi');
 img.onclick = function(){
     var interval = setInterval(moveRight, 10);
-}
+};
+var button = document.getElementById('counter');
+button.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+};
+request.open('GET', 'http://rockeshkumawath26.imad.hasura-app.io/counter', true);
+request.send(null);
