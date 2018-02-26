2e945dd589277709b40d18037a8e904f2f101a82
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-26 20:39:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ app.get('/articles/:articleName', function(req, res){
             }
         }
     });
-    
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
