0d42936cbd2f85f9fc87c18df96e55dc9d6b846a
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-26 21:15:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config = {
     database: 'rockeshkumawath26',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-rockeshkumawath26-13513'
+    password: process.env.DB_PASSWORD
 };
 
 
