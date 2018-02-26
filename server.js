f1f615256c2945f983945e38cc3113fb7bcec795
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-26 20:59:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,9 +69,9 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-/*
-var pool = new Pool(config);
+
 var Pool = require('pq').Pool;
+var pool = new Pool(config);
 app.get('/articles/:articleName', function(req, res){
     pool.query("SELECT * FROM ARTICLES WHERE TITLE = $1", [req.params.articleName], function(err, result){
         if(err){
@@ -88,7 +88,7 @@ app.get('/articles/:articleName', function(req, res){
 });
 // This can be hacked like this: /articles/';delete where 'a'='a 
 // so we need to use parametized input. i.e., it will take ';delete...' as an article name
-*/
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
