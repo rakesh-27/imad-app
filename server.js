1967751cfe31abef0069961a586e816ac5069fd6
rakesh-27 <30873433+rakesh-27@users.noreply.github.com>
2018-02-26 20:33:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pq').Pool;
+var config = {
+    user : 'rockeshkumawath26',
+    database: 'rockeshkumawath26',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+var pool = new Pool(config);
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -58,6 +68,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articles/:articleName', function(req, res){
+    
+    pool.query("SELECT * FROM ARTICLES WHERE TITLE = " + "'" + req.params.articleName + "'", function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0){
+                res.status(404).send("Article not found");
+            } else {
+                var articleData = result.row[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -78,12 +104,6 @@ var counter = 0;
 app.get('/counter', function(req, res){
    counter = counter + 1;
    res.send(counter.toString());
-});
-
-
-app.get('/:articleName', function(req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/main.js', function (req, res) {
